1cf13906279f43ab085295048eeca46db95302ec
Changhwan Sung <chsung@c1r5s1.42gyeongsan.kr>
2024-08-20 17:06:46 +0900

    chsung

--- chsung/queue.h
+++ chsung/queue.h
@@ -1,24 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   solve.c                                            :+:      :+:    :+:   */
+/*   queue.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: chsung <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/20 15:24:49 by chsung            #+#    #+#             */
-/*   Updated: 2024/08/20 15:24:51 by chsung           ###   ########.fr       */
+/*   Created: 2024/08/20 16:49:14 by chsung            #+#    #+#             */
+/*   Updated: 2024/08/20 16:49:15 by chsung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "solve.h"
+#ifndef QUEUE_H
+# define QUEUE_H
+# include "utils.h"
 
-char	**solve(const char *constrains)
+typedef struct s_node
 {
-	int		i;
-	char	**board;
+	t_node	*prev;
+	t_node	*next;
+	char	borad[MAX_SIZE][MAX_SIZE];
+}	t_node;
 
-	board = malloc(sizeof(char *) * MAX_SIZE);
-	i = -1;
-	while (++i < MAX_SIZE)
-		board[i] = malloc(sizeof(char) * MAX_SIZE);
-}
+typedef struct s_queue
+{
+	struct s_node	*first;
+	struct s_node	*last;
+	unsigned int	len;
+}	t_queue;
+
+t_queue	*new_queue(void);
+void	q_put(t_queue *q, t_node *n);
+t_node	*q_get(t_queue *q);
+#endif
--- chsung/solve.h
+++ chsung/solve.h
@@ -9,3 +9,10 @@
 /*   Updated: 2024/08/20 15:24:43 by chsung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef SOLVE_H
+# define SOLVE_H
+# include "utils.h"
+
+char	**solve(const char *constrains);
+#endif
