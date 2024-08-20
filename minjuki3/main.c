fdc576582bca27ea3e502c8e0c61dffb02ab81d3
Minjun Kim <minjuki3@c2r2s2.42gyeongsan.kr>
2024-08-20 23:00:11 +0900

    feat: add conditional statement checking rowleft,columnup

--- minjuki3/main.c
+++ minjuki3/main.c
@@ -6,7 +6,7 @@
 /*   By: minjuki3 <minjuki3@student.42gyeongsa      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 17:02:34 by minjuki3          #+#    #+#             */
-/*   Updated: 2024/08/20 21:41:19 by minjuki3         ###   ########.fr       */
+/*   Updated: 2024/08/20 22:57:50 by minjuki3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,12 @@
 #define SIZE 4
 
 void	print_board(const char board[SIZE][SIZE]);
+int		is_valid(char board[SIZE][SIZE], int row, int col, int num,
+			int input[SIZE][SIZE]);
 
-void	dfs(char board[SIZE][SIZE], int row, int col)
+void	dfs(char board[SIZE][SIZE], int row, int col, int input[SIZE][SIZE])
 {
 	int	num;
-	int	i;
-	int	is_exist;
 	int	next_row;
 	int	next_col;
 
@@ -38,31 +38,75 @@ void	dfs(char board[SIZE][SIZE], int row, int col)
 	}
 	if (board[row][col] != '0')
 	{
-		dfs(board, next_row, next_col);
+		dfs(board, next_row, next_col, input);
 		return ;
 	}
 	num = 0;
-	while (++num <= 4)
+	while (++num <= SIZE)
 	{
-		i = 0;
-		is_exist = 0;
-		while (i < SIZE)
+		if (is_valid(board, row, col, num, input))
 		{
-			if (board[row][i] == (num + '0') || board[i][col] == (num + '0'))
-			{
-				is_exist = 1;
-				break ;
-			}
-			i++;
+			board[row][col] = num + '0';
+			dfs(board, next_row, next_col, input);
+			board[row][col] = '0';
 		}
-		if (is_exist)
-			continue ;
-		board[row][col] = num + '0';
-		dfs(board, next_row, next_col);
-		board[row][col] = '0';
 	}
 }
 
+int	is_valid(char board[SIZE][SIZE], int row, int col, int num,
+		int input[SIZE][SIZE])
+{
+	int	i;
+	int	max;
+	int	cnt;
+
+	i = 0;
+	while (i < SIZE)
+	{
+		if (board[row][i] == (num + '0') || board[i][col] == (num + '0'))
+		{
+			return (0);
+		}
+		i++;
+	}
+	i = 0;
+	max = 0;
+	cnt = 0;
+	board[row][col] = num + '0';
+	while (i < SIZE) // l ->r
+	{
+		if (board[row][i] > max)
+		{
+			max = board[row][i];
+			cnt++;
+		}
+		i++;
+	}
+	if (cnt > input[2][row])
+	{
+		board[row][col] = '0';
+		return (0);
+	}
+	i = 0;
+	max = 0;
+	cnt = 0;
+	while (i < SIZE) // t ->b
+	{
+		if (board[i][col] > max)
+		{
+			max = board[row][i];
+			cnt++;
+		}
+		i++;
+	}
+	if (cnt > input[0][col])
+	{
+		board[row][col] = '0';
+		return (0);
+	}
+	board[row][col] = '0';
+	return (1);
+}
 void	print_board(const char board[SIZE][SIZE])
 {
 	int	i;
@@ -88,9 +132,11 @@ void	print_board(const char board[SIZE][SIZE])
 
 int	main(void)
 {
-	char	board[SIZE][SIZE] = {{'4', '0', '0', '0'}, {'0', '4', '0', '0'},
-			{'0', '0', '4', '0'}, {'0', '0', '0', '4'}};
+	char	board[SIZE][SIZE] = {{'0', '0', '0', '4'}, {'0', '0', '4', '0'},
+			{'0', '4', '0', '0'}, {'4', '0', '0', '0'}};
+	int		input[SIZE][SIZE] = {{4, 3, 2, 1}, {1, 2, 2, 2}, {4, 3, 2, 1}, {1,
+				2, 2, 2}};
 
-	dfs(board, 0, 0);
+	dfs(board, 0, 0, input);
 	return (0);
 }
