3b8abe4f43d8626440945de32866bf3cca923c88
Jiyun Lee <jilee2@c1r10s2.42gyeongsan.kr>
2024-08-20 15:25:12 +0900

    mkdir Jiyun

---
+++ jiyun/main.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jilee2 <devcarrot12@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/20 14:52:05 by jilee2            #+#    #+#             */
+/*   Updated: 2024/08/20 15:24:08 by jilee2           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+ 	Rush01 - skyscrapers N x N
+	dev: jilee2, yuskim, chsung, minjuki3
+	2024. 8. 20 (Tue)
+ */
+
+int	main(int argc, char **argv)
+{
+	char	**condition;
+	char	**state;
+	int		i;
+
+	if (argc != 2)
+	{
+		// write로 바꾸기
+		printf ("error\n");
+		return (1);
+	}
+	i = 0;
+	while (argv[1][i])
+	{
+		
+		i++;
+	}
+	return (0);
+}
