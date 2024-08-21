98cc086eeb417f026ef32db4365a6b90f2d5acb1
Changhwan Sung <chsung@c1r1s6.42gyeongsan.kr>
2024-08-21 16:02:23 +0900

    chsung

--- chsung/check.c
+++ chsung/check.c
@@ -17,21 +17,23 @@ int	is_invaild_row_left(const t_node *n, const int i)
 	int	j;
 	int	c;
 	int	prev;
+	int	constrain;
 
 	j = -1;
 	c = 0;
 	prev = 0;
+	constrain = n->constrains[2 * MAX_SIZE + i] - '0';
 	while (++j < MAX_SIZE)
 	{
+		if (n->board[i][j] == 0)
+			break ;
 		if (prev < n->board[i][j])
 		{
 			prev = n->board[i][j];
 			c++;
 		}
 	}
-	if ((c > n->constrains[2 * MAX_SIZE + i] - '0') \
-	|| ((prev == '0' + MAX_SIZE) \
-		&& (c < n->constrains[2 * MAX_SIZE + i] - '0')))
+	if ((c > constrain) || ((prev == '0' + MAX_SIZE) && (c < constrain)))
 		return (1);
 	return (0);
 }
@@ -41,21 +43,23 @@ int	is_invaild_row_right(const t_node *n, const int i)
 	int	j;
 	int	c;
 	int	prev;
+	int	constrain;
 
 	j = MAX_SIZE;
 	c = 0;
 	prev = 0;
+	constrain = n->constrains[3 * MAX_SIZE + i] - '0';
 	while (--j >= 0)
 	{
+		if (n->board[i][j] == 0)
+			break ;
 		if (prev < n->board[i][j])
 		{
 			prev = n->board[i][j];
 			c++;
 		}
 	}
-	if ((c > n->constrains[3 * MAX_SIZE + i] - '0') \
-	|| ((prev == '0' + MAX_SIZE) \
-		&& (c < n->constrains[3 * MAX_SIZE + i] - '0')))
+	if ((c > constrain) || ((prev == '0' + MAX_SIZE) && (c < constrain)))
 		return (1);
 	return (0);
 }
@@ -65,20 +69,23 @@ int	is_invaild_col_up(const t_node *n, const int j)
 	int	i;
 	int	c;
 	int	prev;
+	int	constrain;
 
 	i = -1;
 	c = 0;
 	prev = 0;
+	constrain = n->constrains[j] - '0';
 	while (++i < MAX_SIZE)
 	{
+		if (n->board[i][j] == 0)
+			break ;
 		if (prev < n->board[i][j])
 		{
 			prev = n->board[i][j];
 			c++;
 		}
 	}
-	if ((c > n->constrains[j] - '0') \
-	|| ((prev == '0' + MAX_SIZE) && (c < n->constrains[j] - '0')))
+	if ((c > constrain) || ((prev == '0' + MAX_SIZE) && (c < constrain)))
 		return (1);
 	return (0);
 }
@@ -88,30 +95,33 @@ int	is_invaild_col_down(const t_node *n, const int j)
 	int	i;
 	int	c;
 	int	prev;
+	int	constrain;
 
 	i = MAX_SIZE;
 	c = 0;
 	prev = 0;
+	constrain = n->constrains[MAX_SIZE + j] - '0';
 	while (--i >= 0)
 	{
+		if (n->board[i][j] == 0)
+			break ;
 		if (prev < n->board[i][j])
 		{
 			prev = n->board[i][j];
 			c++;
 		}
 	}
-	if ((c > n->constrains[MAX_SIZE + j] - '0') \
-	|| ((prev == '0' + MAX_SIZE) && (c < n->constrains[MAX_SIZE + j] - '0')))
+	if ((c > constrain) || ((prev == '0' + MAX_SIZE) && (c < constrain)))
 		return (1);
 	return (0);
 }
 
 int	is_invalid_placement(t_node *n, t_coordinate c)
 {
-	return (is_duplicate_row(n) || is_duplicate_col(n));
-	return (is_duplicate_row(n) || is_duplicate_col(n) \
-		|| is_invaild_col_up(n, c.j) || is_invaild_row_left(n, c.i));
-	// return (is_duplicate_row(n) || is_duplicate_col(n) \
-	// 	|| is_invaild_col_up(n, c.j) || is_invaild_col_down (n, c.j) \
-	// 	|| is_invaild_row_left(n, c.i) || is_invaild_row_right(n, c.i));
+	// return (is_duplicate_row(n, c) || is_duplicate_col(n, c));
+	// return (is_duplicate_row(n, c) || is_duplicate_col(n, c) \
+	// 	|| is_invaild_col_up(n, c.j) || is_invaild_row_left(n, c.i));
+	return (is_duplicate_row(n, c) || is_duplicate_col(n, c) \
+		|| is_invaild_col_up(n, c.j) || is_invaild_col_down (n, c.j) \
+		|| is_invaild_row_left(n, c.i) || is_invaild_row_right(n, c.i));
 }
